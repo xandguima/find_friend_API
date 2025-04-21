2982350e5d0b8cfc683d5806e768ac07c8b21fce
Alexandre <devxandeapp@gmail.com>
2025-04-21 00:18:29 -0300

    Add: Node_env

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,7 +2,8 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
-  PORT:z.coerce.number().default(3333)
+  PORT:z.coerce.number().default(3333),
+  NODE_ENV:z.enum(['dev','test','production']).default('dev'),
 })
 
 const _env = envSchema.safeParse(process.env)
